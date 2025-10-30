fb04e662c413bcdeffc04be0d1a932f470b8c982
Raphdf201 <raphdf201@hotmail.com>
2025-10-30 14:34:19 -0400

    Remove tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,4 @@ dependencies {
     implementation(libs.ktor.server.netty)
     implementation(libs.logback.classic)
     implementation(libs.ktor.server.config.yaml)
-    testImplementation(libs.ktor.server.test.host)
-    testImplementation(libs.kotlin.test.junit)
 }
