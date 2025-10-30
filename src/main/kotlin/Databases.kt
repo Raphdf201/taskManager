8a2b7abaa653bca0e21a3f9f8c6e99de6b7bf991
Raphdf201 <raphdf201@hotmail.com>
2025-10-30 17:16:51 -0400

    Prod ip

--- src/main/kotlin/Databases.kt
+++ src/main/kotlin/Databases.kt
@@ -10,7 +10,7 @@ import org.jetbrains.exposed.sql.Database
 fun Application.configureDatabases() {
     val database =
             Database.connect(
-                url = "jdbc:mariadb://raw.raphdf201.net:3306/taskmanager",// prod ip 192.168.1.14
+                url = "jdbc:mariadb://localhost:3306/taskmanager",
                 user = "taskmgr",
                 driver = "org.mariadb.jdbc.Driver",
                 password = "TaskManagerPw124!",
@@ -54,6 +54,12 @@ fun Application.configureDatabases() {
         delete("/users/{id}") {
             val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
             databaseService.deleteUser(id)
+            call.respond(HttpStatusCode.OK)
+        }
+
+        delete("/tasks/{id}") {
+            val id = call.parameters["id"]?.toInt() ?: throw IllegalArgumentException("Invalid ID")
+            databaseService.deleteTask(id)
             call.respond(HttpStatusCode.OK)
         }
     }
