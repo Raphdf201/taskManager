f856a8b751140d0be2df55129786a25bf7d9b311
Raphdf201 <raphdf201@hotmail.com>
2025-11-01 20:42:34 -0400

    Enable security

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -9,7 +9,7 @@ fun main(args: Array<String>) {
 
 fun Application.module() {
     configureDatabases()
-//    configureSecurity()
+    configureSecurity()
     configureHTTP()
     configureRouting()
 }
