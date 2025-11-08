1287b602191368b43b7ec0c67799531588b1fc46
samytahiri <samy.tahiri26@gmail.com>
2025-11-08 18:54:28 -0500

    adding wave background

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,31 @@
 import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
+import Aurora from './Components/Aurora'
 
-createRoot(document.getElementById('root')!).render(
-    <StrictMode>
-        <App/>
-    </StrictMode>,
-)
+const rootElement = document.getElementById('root')
+if (rootElement) {
+    createRoot(rootElement).render(
+        <StrictMode>
+            <div style={{
+                width: '100vw',
+                height: '100vh',
+                position: 'relative',
+                backgroundColor: '#000000'
+            }}>
+                <div style={{ position: 'absolute', inset: 0 }}>
+                    <Aurora
+                        colorStops={["#ffff2e", "#ed8ef5", "#000000"]}
+                        blend={0.5}
+                        amplitude={1.0}
+                        speed={0.5}
+                    />
+                </div>
+                <div style={{ position: 'relative', zIndex: 1 }}>
+                    <App/>
+                </div>
+            </div>
+        </StrictMode>
+    )
+}
