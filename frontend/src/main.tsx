559577e3c5c141e129e2306c9ca4f67ed3ca266f
samytahiri <samy.tahiri26@gmail.com>
2025-11-08 20:45:27 -0500

    adding home text

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import {createRoot} from 'react-dom/client'
 import './index.css'
 import App from './App'
 import Aurora from './Components/Aurora'
+import HomeText from './Components/HomeText' // Add this import
 
 const rootElement = document.getElementById('root')
 if (rootElement) {
@@ -14,6 +15,7 @@ if (rootElement) {
                 position: 'relative',
                 backgroundColor: '#000000'
             }}>
+                {/* Aurora Background */}
                 <div style={{ position: 'absolute', inset: 0 }}>
                     <Aurora
                         colorStops={["#ffff2e", "#ed8ef5", "#000000"]}
@@ -22,7 +24,32 @@ if (rootElement) {
                         speed={0.5}
                     />
                 </div>
-                <div style={{ position: 'relative', zIndex: 1 }}>
+                
+                {/* Content Layer */}
+                <div style={{ 
+                    position: 'relative', 
+                    zIndex: 1,
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                }}>
+                    <HomeText
+                        text={[
+                            "Bonjour, Samy!",
+                            "Bienvenue chez 3990!",
+                        ]}
+                        as="h1"
+                        typingSpeed={80}
+                        deletingSpeed={40}
+                        pauseDuration={4000}
+                        loop={true}
+                        showCursor={true}
+                        textColors={["#ffffffff", "#fffffff", "#ffffff"]}
+                        className="centered-text"
+                    />
                     <App/>
                 </div>
             </div>
