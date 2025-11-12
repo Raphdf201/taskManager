a7a3b7901e91fae67b01479d94d0bbb1caa47018
Raphdf201 <raphdf201@hotmail.com>
2025-11-12 15:54:42 -0500

    Change login redirect url

--- src/main/kotlin/Auth.kt
+++ src/main/kotlin/Auth.kt
@@ -121,13 +121,13 @@ fun Application.configureSecurity() {
 
                         val u = db.getUser(userInfo.id)
                         if (u == null) db.createUser(ExposedUser(userInfo.id, userInfo.name, userInfo.picture))
-                        call.respondRedirect(Constants.Static.DEV_FRONTEND)
+                        call.respondRedirect(Constants.Static.DEV_FRONTEND + "?auth=success")
                         return@get
                     } catch (e: Exception) {
                         log("Error at ${Clock.System.now()} : dev login ${e.message} ${e.stackTrace}")
                     }
                 }
-                call.respondRedirect(Constants.Static.DEV_FRONTEND)
+                call.respondRedirect(Constants.Static.DEV_FRONTEND + "?auth=failure")
             }
         }
 
