efbfc98a297889f3e57ca93e97a0337896949f62
Samy <samy.tahiri26@gmail.com>
2026-02-23 22:37:34 -0500

    started menu

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
+import Menu from './pages/Menu/Menu'
+import Tasks from './pages/Tasks/Tasks'
+import Members from './pages/Members/Members'
+
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Menu />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/tasks" element={<Tasks />} />
+        <Route path="/members" element={<Members />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ frontend/src/pages/Menu/Menu.tsx
@@ -0,0 +1,51 @@
+import './Menu.css'
+import { useState, useEffect } from 'react'
+import {Link} from 'react-router-dom'
+
+function Menu() {
+
+  const [userName, setUserName] = useState<string>('Samy')
+  const [displayText, setDisplayText] = useState<string>('')
+  const [showCursor, setShowCursor] = useState<boolean>(true)
+  const fullText = "Bonjour,\nSamy"
+  
+ useEffect(() => {
+    let index = 0
+    const interval = setInterval(() => {
+      if (index < fullText.length) {
+        setDisplayText(fullText.slice(0, index + 1))
+        index++
+      } else {
+        clearInterval(interval)
+      }
+    }, 70) 
+
+    return () => clearInterval(interval)
+  }, [fullText])
+
+  useEffect(() => {
+    const cursorInterval = setInterval(() => {
+      setShowCursor((prev) => !prev)
+    }, 500)
+
+    return () => clearInterval(cursorInterval)
+  }, [])
+
+
+  return (
+    <div className='menu-page'>
+     <div className="top-line"></div>
+     <nav className="navbar">
+     <Link to="/menu">Menu</Link>
+     <Link to="/tasks">Tâches</Link>
+     <Link to="/members">Membres</Link>
+     </nav>
+    <h1 className='welcome-text' style = {{whiteSpace: 'pre-line'}}>
+    {displayText}
+    {showCursor && <span className = "cursor">|</span>}
+    </h1>
+    </div>
+  )
+}
+
+export default Menu
